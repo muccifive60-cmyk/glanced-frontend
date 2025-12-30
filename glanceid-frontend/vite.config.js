0ed02ade2fb8e8b93688d0a80369d587ae811df7
muccifive60-cmyk <muccifive60@icloud.com>
2025-12-29 22:58:33 -0800

    Fix Safari white screen

--- glanceid-frontend/vite.config.js
+++ glanceid-frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+  },
+  server: {
+    host: true
+  }
 })
